8ff6b036b1f87ddc074ae6a0238cf0be166187c3
Craig Chambers <cchambers@salisbury6c.ac.uk>
2020-05-02 16:00:51 +0100

    Initial install

---
+++ install.sh
@@ -0,0 +1,42 @@
+#/bin/bash!
+
+clear
+
+echo 'Installing useful software for your course ... '
+echo
+echo
+echo -n 'Installing Figlet ... '
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq figlet < /dev/null > /dev/null
+echo 'Complete!'
+echo
+echo -n 'Installing Open Broadcaster Software (OBS) ... '
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq ffmpeg obs-studio < /dev/null > /dev/null
+echo 'Complete!'
+echo
+echo -n 'Installing Chromium ... '
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq chromium-browser < /dev/null > /dev/null
+echo 'Complete!'
+echo
+echo -n 'Installing Video 4 Linux Utilities (to control webcams) ... '
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq v4l-utils < /dev/null > /dev/null
+echo 'Complete!'
+echo
+echo -n 'Installing Openshot Video Editor ... '
+sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:openshot.developers/ppa > /dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq openshot-qt < /dev/null > /dev/null
+echo 'Complete!'
+echo
+echo -n 'Installing GNU Image Manipulation Software (GIMP) ... '
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq gimp < /dev/null > /dev/null
+echo 'Complete!'
+echo
+echo -n 'Installing Inkscape ... '
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq inkscape < /dev/null > /dev/null
+echo 'Complete!'
+echo
+echo -n 'Installing Youtube downloader ... '
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq youtube-dl < /dev/null > /dev/null
+echo 'Complete!'
+echo
+clear
+figlet 'All Applications are now installed'
